c8445e65682ca42dc88ced91e162aefb383c1f71
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-05-18 07:41:22 +0000

    Adding a subscriber to be used by offical model repository to instantiate the correct theme.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/cellml.themechooser/trunk@4970 eb92df5b-a40c-0410-b02c-bf52feaaa166

---
+++ cellml/themechooser/subscriber.py
@@ -0,0 +1,14 @@
+from Products.CMFCore.utils import getToolByName
+
+skins = {
+    'models.cellml.org': 'CellML Theme (CellML)',
+    'models.physiomeproject.org': 'CellML Theme (PMR2)',
+}
+
+def select_theme(obj, event):
+    server_url = event.request['SERVER_URL']
+    host = server_url.split('://')[1].split(':')[0]
+    skin = skins.get(host)
+    if skin:
+        site = getToolByName(obj, 'portal_url').getPortalObject()
+        site.changeSkin(skin, event.request)
